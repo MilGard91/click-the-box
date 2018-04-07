62a3b92bc0a8cb11939704470d120dfe7f3d5ccf
Miljan Milidrag <m.milidrag25@gmail.com>
2018-04-07 12:55:26 +0200

    algorithms stage 1-1

--- src/components/Boxes/Boxes.js
+++ src/components/Boxes/Boxes.js
@@ -2,32 +2,21 @@ import React from 'react';
 
 import Box from './Box/Box';
 import classes from './Boxes.css';
+import {isItInArray} from '../../Utility';
 
 const boxes = (props) => {
     let fields = [];
     for (let i = 0; i < 10; i++) {
         fields.push([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     }
-    const searchForArray = (haystack, needle) => {
-        let i, j, current;
-        for (i = 0; i < haystack.length; i++) {
-            if (needle.length === haystack[i].length) {
-                current = haystack[i];
-                for (j = 0; j < needle.length && needle[j] === current[j]; ++j);
-                if (j === needle.length)
-                    return true;
-            }
-        }
-        return false;
-    }
-
+    console.log(props.must)
     const fieldsBoard = fields.map((i, j) => (
         i.map(z => (
-            searchForArray(props.active, [j, z]) ?
+            isItInArray(props.active, [j, z]) ?
                 <Box active key={j + '-' + z} row={j} col={z} clicked={(x, y) => (props.clicked(x, y))} /> :
-                searchForArray(props.next, [j, z]) ?
+                isItInArray(props.next, [j, z]) ?
                     <Box next key={j + '-' + z} row={j} col={z} clicked={(x, y) => (props.clicked(x, y))} /> :
-                    searchForArray(props.must, [j, z]) ?
+                    isItInArray(props.must, [j, z]) ?
                         <Box must key={j + '-' + z} row={j} col={z} clicked={(x, y) => (props.clicked(x, y))} /> :
                         <Box key={j + '-' + z} row={j} col={z} clicked={(x, y) => (props.clicked(x, y))} />
         ))
--- src/store/reducers/board.js
+++ src/store/reducers/board.js
@@ -1,40 +1,32 @@
 import * as actionTypes from '../actions/actionTypes';
+import * as algorithms from './algorithms';
 
 const intitialState = {
-    startPosition: [],
     nextPosition: [],
-    activePosition: [] ,
+    activePosition: [],
     mustPosition: [],
     lastClicked: [],
-    level=1,
+    level: 10,
 }
 
-const directions = [[-3,0], [-2, 2], [0,3] [2,2], [3,0], [2,-2], [0,3], [-2 ,-2]];
-
-const addingField = (cur, move) => {
-    let moved = [cur[0]+move[0], cur[1]+move[1]];
-    return moved;
-}
-const levelBuilder = (lvl, startPosition) => {
-    
-    for (let i=0; i<lvl; i++) {
-        directions[Math.floor(Math.random() * 8)]
-    }
-}
 
 const reducer = (state = intitialState, action) => {
     switch (action.type) {
         case actionTypes.GAME_START:
+            let must = algorithms.gameBuild(state.level, [[action.activePositionX, action.activePositionY]]);
             return {
                 ...state,
-                startPosition:[[action.activePositionX, action.activePositionY]],
                 activePosition: [[action.activePositionX, action.activePositionY]],
-                nextPosition: [[action.activePositionX +1, action.activePositionY + 2]]
+                mustPosition: must,
+                nextPosition: algorithms.nextMoves([[action.activePositionX, action.activePositionY]], must),
             };
         case actionTypes.BOX_CLICKED:
+            let newMust = algorithms.positionClicked( [[action.activePositionX, action.activePositionY]], state.mustPosition)
             return {
                 ...state,
-                activePosition: [...state.activePosition, [action.activePositionX, action.activePositionY]]
+                activePosition: [...state.activePosition, [action.activePositionX, action.activePositionY]],
+                mustPosition: newMust,
+                nextPosition: algorithms.nextMoves([[action.activePositionX, action.activePositionY]], newMust),
             }
 
         default: return state;
