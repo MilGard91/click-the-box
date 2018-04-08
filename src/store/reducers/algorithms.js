163966902d71d14b489f130512674ff1f1528b44
Miljan Milidrag <m.milidrag25@gmail.com>
2018-04-08 11:24:03 +0200

    ubrzan algoritam za lvl preko 86

--- src/store/reducers/algorithms.js
+++ src/store/reducers/algorithms.js
@@ -5,7 +5,37 @@ const d = [[-3, 0], [-2, 2], [0, 3], [2, 2], [3, 0], [2, -2], [0, -3], [-2, -2]]
 
 export const gameBuild = (lvl, start) => {
 
-    const check = (res) => {
+    if (lvl < 87) {
+        const check = (res) => {
+            if (res.length - 1 === lvl &&
+                !(res[res.length - 1][0] < 0) &&
+                !(res[res.length - 1][0] >= 10) &&
+                !(res[res.length - 1][1] < 0) &&
+                !(res[res.length - 1][1] >= 10) &&
+                !isItInArray(res.slice(0, res.length - 1), res[res.length - 1])) {
+                return res.slice(1);
+            }
+            else if (res[res.length - 1][0] < 0 ||
+                res[res.length - 1][0] >= 10 ||
+                res[res.length - 1][1] < 0 ||
+                res[res.length - 1][1] >= 10 ||
+                isItInArray(res.slice(0, res.length - 1), res[res.length - 1])) {
+                return null;
+            } else {
+                let r = Math.floor(Math.random() * 8);
+                return check(res.concat([[res[res.length - 1][0] + d[r][0], res[res.length - 1][1] + d[r][1]]])) ||
+                    check(res.concat([[res[res.length - 1][0] + d[0][0], res[res.length - 1][1] + d[0][1]]])) ||
+                    check(res.concat([[res[res.length - 1][0] + d[1][0], res[res.length - 1][1] + d[1][1]]])) ||
+                    check(res.concat([[res[res.length - 1][0] + d[2][0], res[res.length - 1][1] + d[2][1]]])) ||
+                    check(res.concat([[res[res.length - 1][0] + d[3][0], res[res.length - 1][1] + d[3][1]]])) ||
+                    check(res.concat([[res[res.length - 1][0] + d[4][0], res[res.length - 1][1] + d[4][1]]])) ||
+                    check(res.concat([[res[res.length - 1][0] + d[5][0], res[res.length - 1][1] + d[5][1]]])) ||
+                    check(res.concat([[res[res.length - 1][0] + d[6][0], res[res.length - 1][1] + d[6][1]]])) ||
+                    check(res.concat([[res[res.length - 1][0] + d[7][0], res[res.length - 1][1] + d[7][1]]]))
+            }
+        };
+        return check([...start]);
+    } else {const check = (res) => {
         if (res.length - 1 === lvl &&
             !(res[res.length - 1][0] < 0) &&
             !(res[res.length - 1][0] >= 10) &&
@@ -21,9 +51,7 @@ export const gameBuild = (lvl, start) => {
             isItInArray(res.slice(0, res.length - 1), res[res.length - 1])) {
             return null;
         } else {
-            let r = Math.floor(Math.random() * 8);
-            return check(res.concat([[res[res.length - 1][0] + d[r][0], res[res.length - 1][1] + d[r][1]]])) ||
-                check(res.concat([[res[res.length - 1][0] + d[0][0], res[res.length - 1][1] + d[0][1]]])) ||
+            return check(res.concat([[res[res.length - 1][0] + d[0][0], res[res.length - 1][1] + d[0][1]]])) ||
                 check(res.concat([[res[res.length - 1][0] + d[1][0], res[res.length - 1][1] + d[1][1]]])) ||
                 check(res.concat([[res[res.length - 1][0] + d[2][0], res[res.length - 1][1] + d[2][1]]])) ||
                 check(res.concat([[res[res.length - 1][0] + d[3][0], res[res.length - 1][1] + d[3][1]]])) ||
@@ -33,7 +61,7 @@ export const gameBuild = (lvl, start) => {
                 check(res.concat([[res[res.length - 1][0] + d[7][0], res[res.length - 1][1] + d[7][1]]]))
         }
     };
-    return check([...start]);
+    return check([...start]);}
 };
 
 export const nextMoves = (last, must) => {
@@ -50,10 +78,10 @@ export const nextMoves = (last, must) => {
 };
 
 export const positionClicked = (last, must) => {
-    let newMust= [...must];
+    let newMust = [...must];
     let k = positionInArray(newMust, last);
-    newMust.splice(k,1);
-  return newMust;
+    newMust.splice(k, 1);
+    return newMust;
 }
 
 
